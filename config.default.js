ff304e4f6950d79e9836920a37e09dd0ec001e0d
Anthony Nguyen <anknguyen@gmail.com>
2014-06-14 09:28:21 -0400

    Updated config file

--- config.default.js
+++ config.default.js
@@ -1,1 +1,19 @@
-logPath = ""
+config = {};
+config.logPath = "";
+
+config.exposeEvents = {
+	"InitGame": true,
+	"InitRound": true,
+	"InitAuth": true,
+	"ShutdownGame": true,
+	"ClientConnect": true,
+	"ClientDisconnect": true,
+	"ClientUserinfo": true,
+	"ClientSpawn": true,
+	"ClientBegin": true,
+	"say": true,
+	"sayteam": true,
+	"Kill": true,
+	"SurvivorWinner": true
+}
+	
